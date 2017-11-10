0f24f2720cb138c313e7ab1e9f6043add0ef3c56
TaodongWu <ltfy_xkfh@yeah.net>
2017-11-10 17:31:24 +0800

    完成connect内容，增加channel内容

--- test.js
+++ test.js
@@ -1,2 +1,15 @@
-let str = '店达商城';
-console.log(Buffer.from(str).toString('base64'))
+let num = 1;
+
+class Test {
+    constructor() {
+        this.num = 0;
+    }
+    test1() {
+        this.test2();
+    }
+    test2() {
+        console.log(this.num);
+    }
+}
+
+(new Test()).test1();
