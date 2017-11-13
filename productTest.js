f5bc13950da6e990b9a28d677b934c597db8e220
TaodongWu <ltfy_xkfh@yeah.net>
2017-11-13 18:43:06 +0800

    完成了发送消息和接收消息等操作

--- productTest.js
+++ productTest.js
@@ -1,24 +1,22 @@
-var config = {
-        host: '192.168.1.101:5674',
-        account: 'guest',
-        password: 'guest'
-    },
-    options = {
-        queueName: 'test-queue',
-        prefetchCount: 1
-    },
-    Queue = require('./index').Queue,
-    i = 0;
-var queue = new Queue(config, options);
+const Queue = require('./lib/queue');
+// const Channel = require('./lib/channel');
+const Connect = require('./lib/connect');
+
+let conn = new Connect({
+    host: '192.168.1.101',
+    port: '5672',
+    accont: 'guest',
+    password: 'guest'
+});
+
+let queue = new Queue('test', conn);
+let n = 0;
 
 setInterval(function () {
-    i++;
-    console.log(i);
-    queue.sendToQueue(i)
-        .then(function(e) {
-            console.log(e);
-        })
-        .catch(function(e) {
-            console.log(e);
-        })
-}, 500);
+    queue.sendToQueue({
+        n: n
+    });
+    n++;
+}, 1000)
+
+// console.log(Buffer.from('1233123') instanceof Buffer);
--- test.js
+++ test.js
@@ -1,15 +1,25 @@
-let num = 1;
+const Queue = require('./lib/queue');
+const Connect = require('./lib/connect');
 
-class Test {
-    constructor() {
-        this.num = 0;
-    }
-    test1() {
-        this.test2();
-    }
-    test2() {
-        console.log(this.num);
-    }
-}
+let conn = new Connect({
+    host: '192.168.1.101',
+    port: '5672',
+    accont: 'guest',
+    password: 'guest'
+});
 
-(new Test()).test1();
+let queue = new Queue('test', conn);
+let n = 0;
+
+queue.consume(function (msg) {
+        console.log(n + '------', msg.data);
+        n++;
+        setTimeout(() => {
+            queue.ack(msg);
+        }, 1000);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+
+// console.log(Buffer.from('1233123') instanceof Buffer);
